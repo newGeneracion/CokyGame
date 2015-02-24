e203c0901289f09f5760e440db7c0e25ddada163
William <wigame3@gmail.com>
2015-02-24 14:09:45 -0500

    carga inicial

---
+++ coky/desktop/src/com/nng/cokygame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.nng.cokygame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.nng.cokygame.CokyGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CokyGame(), config);
+	}
+}
