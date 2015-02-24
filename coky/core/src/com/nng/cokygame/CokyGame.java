011dbc06f1248d0f68356e8ba3b6542464d8001f
Fabian <hector.akito89@gmail.com>
2015-02-24 14:54:49 -0500

    Fabian

--- coky/core/src/com/nng/cokygame/CokyGame.java
+++ coky/core/src/com/nng/cokygame/CokyGame.java
@@ -13,7 +13,7 @@ public class CokyGame extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("badlogic1.jpg");
 	}
 
 	@Override
